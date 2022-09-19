730af2bb3a74043f5240986b57cc0e5c7dc2ac40
Kaden Dippe <kadendippe@KadenMacBookPro2021-4.local>
2022-09-19 01:12:49 -0700

    first commit, just destroyed pins on impact

---
+++ Assets/Karting/Scripts/Particle_speed.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using KartGame.KartSystems;
+
+public class Particle_speed : MonoBehaviour
+{
+
+    GameObject[] particlePrefab;
+    ArcadeKart[] karts;
+    float baseSimulationSpeed = .81F;
+    float topSimulationSpeed = 1.04F;
+    float finalGravMod = .71F;
+    float startGravMod = 1.5F;
+    float topRateOverTime = 11.1F;
+    float baseRateOverTime = 5;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        particlePrefab = GameObject.FindGameObjectsWithTag("Particles");
+        karts = FindObjectsOfType<ArcadeKart>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // change the speed of the animation based on 
+        foreach (var kart in karts)
+        {
+            //var scalingFactor = kart.LocalSpeed() / kart.baseStats.TopSpeed;
+            //Debug.Log(kart.LocalSpeed());
+            ChangeParticleSpeed(kart.LocalSpeed());
+        }
+    }
+
+    private void ChangeParticleSpeed(float localSpeed)
+    {
+        foreach (var particleSystem in particlePrefab)
+        {
+            var psMain = particleSystem.GetComponent<ParticleSystem>().main;
+            psMain.gravityModifier = startGravMod + (finalGravMod - startGravMod) * localSpeed;
+            psMain.simulationSpeed = baseSimulationSpeed + (topSimulationSpeed - baseSimulationSpeed) * localSpeed;
+
+            var psEm = particleSystem.GetComponent<ParticleSystem>().emission;
+            psEm.rateOverTime = baseRateOverTime + (topRateOverTime - baseRateOverTime) * localSpeed;
+        }
+    }
+}
---
+++ Assets/ModAssets/Scripts/Kart/EmitParticlesOnKartBounce.cs
@@ -0,0 +1,33 @@
+﻿using KartGame.KartSystems;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class EmitParticlesOnKartBounce : MonoBehaviour 
+{
+    ParticleSystem effect;
+    KartBounce vehicleBounce;
+    bool localBounceFlag = false;
+
+    void Awake() 
+    {
+        effect = GetComponent<ParticleSystem>();
+        vehicleBounce = GetComponentInParent<KartBounce>();
+
+        Assert.IsNotNull(vehicleBounce, "This particle should be a child of a VehicleBounce!");
+    }
+
+    void Update()
+    {
+        
+        
+        if (localBounceFlag != vehicleBounce.BounceFlag) {
+            Debug.Log("Bounce flag is true!!!");
+        }
+
+        if (vehicleBounce.BounceFlag)
+        {
+            effect.Play();
+        }
+    }
+}
